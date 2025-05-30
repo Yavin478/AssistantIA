ddc804c72503293daf5f01356e6cbfd4030c79c3
evalero <evan.leroux24@estudiantes.uva.es>
2025-05-30 21:37:15 +1100

    Rajout des changements sur le fichier "Libraries" oublié dans le précédent commit.

--- Libraries.py
+++ Libraries.py
@@ -5,7 +5,10 @@ Fichier pour les importations des librairies externes nécessaires au Projet
 # Pour les appels API
 import requests
 
+# Pour le projet en général
+from dataclasses import dataclass
+
 # Pour l'interface graphique
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QLabel
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QScrollArea,QLabel, QHBoxLayout, QFrame)
